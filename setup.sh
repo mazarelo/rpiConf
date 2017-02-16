57b91c7f63473a96182e293107cadb37d525a71b
mazarelo <jvmazarelo@gmail.com>
2017-02-16 17:13:10 +0000

    test

--- setup.sh
+++ setup.sh
@@ -6,19 +6,19 @@ CONFIGFILES=$HOME/.rpiConf
 DOWNLOADS=$HOME/Downloads
 
 echo "****** Making files executable ******"
-sudo chmod +==x $CONFIGFILES/*.sh
+  sudo chmod +==x $CONFIGFILES/*.sh
 
 #echo "****** Installing Zsh ******"
 #sudo apt-get install zsh
 
 echo "****** Installing Git ******"
-sudo apt-get install git
+  sudo apt-get install git
 
 echo "****** Installing Nginx ******"
   sudo apt-get update
   sudo apt-get install nginx -y
 echo "****** Installing PHP Extensions ******"
-apt-get install -y php5-fpm php5-cli php5-mcrypt php5-curl php5-mysql php-apc 
+  apt-get install -y php5-fpm php5-cli php5-mcrypt php5-curl php5-mysql php-apc 
 
 echo "****** Installing Node ******"
   sudo apt-get update
@@ -26,37 +26,30 @@ echo "****** Installing Node ******"
   sudo apt-get install npm
 
 echo "****** Starting Nginx ******"
-service nginx start
-update-rc.d nginx defaults
+  service nginx start
+  update-rc.d nginx defaults
 
 echo "****** Removing Nginx defaults ******"
-rm /etc/nginx/sites-enabled/default
-rm /etc/nginx/sites-available/default
+  sudo rm /etc/nginx/sites-enabled/default
+  sudo rm /etc/nginx/sites-available/default
 
 echo "****** Installing phpmyadmin ******"
-# install phpmyadmin and give password(s) to installer
-# for simplicity I'm using the same password for mysql and phpmyadmin
-debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
-debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
-apt-get -y install phpmyadmin
+  sudo apt-get -y install phpmyadmin
 
 echo "****** Enable php extensions ******"
-php5enmod mcrypt
+  sudo php5enmod mcrypt
 
 echo "****** Installing Composer ******"
-cd /tmp
-php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
-php composer-setup.php --install-dir=/usr/local/bin --filename=composer
-php -r "unlink('composer-setup.php');"
+  cd /tmp
+  php -r "readfile('https://getcomposer.org/installer');" > composer-setup.php
+  php composer-setup.php --install-dir=/usr/local/bin --filename=composer
+  php -r "unlink('composer-setup.php');"
 
 #echo "****** Symlink PhantomJs ******"
-#ln -sf /vagrant/libs/phantomjs /usr/local/bin/phantomjs
+  #ln -sf /vagrant/libs/phantomjs /usr/local/bin/phantomjs
 
 echo "****** Restarting Services ******"
-service php5-fpm restart && service nginx restart
+  sudo service php5-fpm restart && service nginx restart
 
 #Install No-ip
-#http://www.noip.com/support/knowledgebase/install-ip-duc-onto-raspberry-pi/
+  #http://www.noip.com/support/knowledgebase/install-ip-duc-onto-raspberry-pi/
